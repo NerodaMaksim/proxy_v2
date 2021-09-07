1dda36439800566c0f85da1506403b07d6ea38ee
Maksim <max.nero200022@gmail.com>
2021-09-07 19:32:40 +0300

    Added documentation and some fixes

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM node:alpine
 
 RUN apk update
 RUN apk add scp 
-RUN apk add --update --no-cache sshpass
+RUN apk add --update --no-cache sshpass openssh
 RUN apk add --update nodejs npm
 
 WORKDIR /app
--- script.sh
+++ script.sh
@@ -9,16 +9,16 @@ mkdir /etc/3proxy
 mv bin/3proxy /etc/3proxy/
 cd ~/files${etcNetworkInterfaces}
 systemctl restart networking
-ip -6 addr add 2001:470:8a17:0364:f089:efcb:6c8a:6c8d dev enp0s3
-ip -6 addr add 2001:470:8a17:d32e:2ea0:2498:974a:53cb dev enp0s3
-ip -6 addr add 2001:470:8a17:29c7:a462:e037:ce11:6aa3 dev enp0s3
-ip -6 addr add 2001:470:8a17:ab66:7ae4:d21b:cdfc:5a4e dev enp0s3
-ip -6 addr add 2001:470:8a17:1461:4cfc:7c27:6141:3f9a dev enp0s3
-ip -6 addr add 2001:470:8a17:f72c:0e1e:0844:ba79:ed3b dev enp0s3
-ip -6 addr add 2001:470:8a17:3b09:5325:0c0a:f284:2819 dev enp0s3
-ip -6 addr add 2001:470:8a17:78d2:9854:a8d6:b94b:0284 dev enp0s3
-ip -6 addr add 2001:470:8a17:eb15:d218:3e1f:9f16:fce5 dev enp0s3
-ip -6 addr add 2001:470:8a17:a0a1:4040:93ec:8430:5478 dev enp0s3
+ip -6 addr add 2001:470:8a17:c0d1:8dfa:2db6:ced5:b322 dev enp0s3
+ip -6 addr add 2001:470:8a17:40cd:8551:7c8e:5b46:9043 dev enp0s3
+ip -6 addr add 2001:470:8a17:522e:6ebf:11e3:feaa:8fc1 dev enp0s3
+ip -6 addr add 2001:470:8a17:98c2:7803:25e3:baf5:ae0b dev enp0s3
+ip -6 addr add 2001:470:8a17:aeae:9338:8bf8:5274:94ee dev enp0s3
+ip -6 addr add 2001:470:8a17:fb4b:0130:be88:b50c:dbeb dev enp0s3
+ip -6 addr add 2001:470:8a17:3498:7da8:46d8:4b8b:2efd dev enp0s3
+ip -6 addr add 2001:470:8a17:c6cb:104c:fa9b:ae52:d4d6 dev enp0s3
+ip -6 addr add 2001:470:8a17:f358:5d3f:8cb9:18b8:cf3b dev enp0s3
+ip -6 addr add 2001:470:8a17:90cd:8083:8370:2a4b:c8d8 dev enp0s3
 
 ip -6 addr add 2001:470:8a17::/48 dev enp0s3
 ip -6 route add default via 2001:470:1f06:4db::1 dev he-ipv6
